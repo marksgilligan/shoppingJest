6fff0ff8d630a3ad8fc8ade9b6d8c22b7659df82
marksgilligan <59980448+marksgilligan@users.noreply.github.com>
2020-04-06 14:17:05 +0100

    first push

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+//display all items
+//add to basket button next to each item
+//increment/decrement button to change the qty
+//implement a max qty
+//total price of basket math math
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TEST</h1>
+    </div>
+  );
+}
+
+export default App;
